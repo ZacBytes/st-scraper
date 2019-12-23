ff2f9732d1ec2731a148c6e7a4df9492e6fd6701
Zachary <zacharylimjx@gmail.com>
2019-12-23 13:39:31 +0800

    initial commit

---
+++ st-scraper.js
@@ -0,0 +1,37 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const validUrl = require('valid-url');
+
+exports.ScrapeArticle = function (articleURL) {
+	if (validUrl.isUri(articleURL)){
+		//Proceed with scraping
+
+		request({
+			method: 'GET',
+			url: 'https://www.straitstimes.com/singapore/courts-crime/former-changi-airport-employee-charged-after-receiving-more-than-200000-in'
+		}, (err, res, body) => {
+		
+			if (err) return console.error(err);
+			let $ = cheerio.load(body);
+      
+
+     		var title = $('h1.headline.node-title').text()
+     		var img = $('img.img-responsive').attr('src')
+      		var imgcaption = $('span.caption-text').text()
+      
+			var text = "";
+			  
+     	 	$("div.odd.field-item").each(function(i, item){
+      			text += ($("p", item).text())
+      		});
+      
+    		return [title, text, img, imgcaption];
+
+		})
+		
+
+	} 
+	else {
+		console.error('error', "Invalid articleURL provided to ScrapeArticle function");
+	}
+  };
