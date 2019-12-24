1df9d19e09439ac971b792e6674d6e0d4732546f
Zachary <zacharylimjx@gmail.com>
2019-12-25 01:07:38 +0800

    fixed

--- index.js
+++ index.js
@@ -2,35 +2,41 @@ const cheerio = require('cheerio');
 const request = require('request');
 const validUrl = require('valid-url');
 
-module.exports.ScrapeArticle = function (articleURL) {
+function stscraper () {}
+
+stscraper.prototype.ScrapeArticle = function (articleURL, callback) {
 	if (validUrl.isUri(articleURL)){
 		//Proceed with scraping
-
 		request(articleURL, function (error, response, body) {
 		
 			if (error) return console.error(error);
 			const $ = cheerio.load(body);
-      
 
-     		var title = $('h1.headline.node-title').text()
-     		var img = $('img.img-responsive').attr('src')
-      		var imgcaption = $('span.caption-text').text()
-      
-			var text = "";
-			  
+     		let title = $('h1.headline.node-title').text();
+     		let img = $('img.img-responsive').attr('src');
+			let imgcaption = $('span.caption-text').text();
+			 
+			let text = "";
      	 	$("div.odd.field-item").each(function(i, item){
       			text += ($("p", item).text())
       		});
-      
-    		return [title, text, img, imgcaption];
 
-		})
-		
+			var ArticleData = {};
+			ArticleData.title = title;
+			ArticleData.text = text;
+			ArticleData.img = img;
+			ArticleData.imgcaption = imgcaption;
+			callback(null, ArticleData)
+
+
+		});
 
 	} 
 	else {
-		console.error('error', "Invalid articleURL provided to ScrapeArticle function");
+		callback("Invalid articleURL provided to ScrapeArticle function");
 	}
-  };
+}
 
+
+module.exports = new stscraper()
   
