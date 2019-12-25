16ad4b9927e1cb9a1eeec162e28f0affa2c3b2ff
Zachary <zacharylimjx@gmail.com>
2019-12-25 09:59:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ stscraper.prototype.ScrapeArticle = function (articleURL, callback) {
      		var img = $('img.img-responsive').attr('src');
 			var imgcaption = $('span.caption-text').text();
 			var postdate = $('div.story-postdate').text()
-			var author = $('div.author-field.author-name').find('a').attr('href')
+			var author = $('div.author-field.author-name').find('a').text();
 
 			var text = "";
      	 	$("div.odd.field-item").each(function(i, item){
